004ec0438493c3077fb23b2977b0d75661c7a2de
Nom <mail@mail.fr>
2024-05-15 12:55:11 +0200

    Ajout Update

--- src/Controller/CandidateController.php
+++ src/Controller/CandidateController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Candidate;
+use App\Form\CandidateType;
+use App\Repository\CandidatureRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
--- src/Controller/CandidatureController.php
+++ src/Controller/CandidatureController.php
@@ -2,19 +2,35 @@
 
 namespace App\Controller;
 
+use App\Entity\Candidature;
+use App\Entity\JobOffer;
+use App\Entity\Candidate;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Persistence\ManagerRegistration;
+use Doctrine\ORM\EntityManagerInterface;
 
 class CandidatureController extends AbstractController
 {
-    /**
-     * @Route("/candidature", name="app_candidature")
-     */
-    public function index(): Response
+    # [Route('/candidature/new/{idCandidate}/{idJobOffer}', name: 'app_candidature', methods: ['GET', 'POST'])]
+    public function newCandidature(int $idCandidate, int $idJobOffer, ManagerRegistration $doctrine, EntityManagerInterface $entityManager): Response    
     {
-        return $this->render('candidature/index.html.twig', [
-            'controller_name' => 'CandidatureController',
-        ]);
+        // Récuperer le candidate
+        $emCandidate = $doctrine->getRepository(Candidate::class)->find($idCandidate);
+
+        // Set values in candidature table
+        $candidacy = new Candidature();
+        $candidacy->setCandidate($emCandidate);
+        $candidacy->setJobOffer($emjobOffer);
+        $entityManager->persist($candidature);
+        $entityManager->flush();
+
+        $this->addFlash(
+            'success',
+            'Vous avez postulé à une annonce. Un consultant doit valider votre demande.'
+        );
+
+        return $this->redirectToRoute('app_job_offer_index', [], Response::HTTP_SEE_OTHER);
     }
 }
